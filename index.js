aadbf2f95419e317f096f6f9e573742f6fd77f06
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-23 15:12:36 +0300

    use express.static

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import path from 'path'
 
+const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello ExpressJS!!!</h1>')
+app.use(express.static(path.resolve(__dirname, 'static')))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+app.get('/download', (req, res) => {
+  res.download(path.resolve(__dirname, 'static', 'index.html'))
 })
 
 app.listen(PORT, () => {
