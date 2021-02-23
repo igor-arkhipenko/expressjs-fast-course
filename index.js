954299406aa9064a38a71fd3e5034a0f8924e8b3
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-23 16:17:36 +0300

    ejs

--- index.js
+++ index.js
@@ -6,18 +6,29 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(requestTime)
 app.use(logger)
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Main Page', active: 'main' })
+})
+
+app.get('/features', (req, res) => {
+  res.render('features', { title: 'Features Page', active: 'features' })
+})
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 // })
 
-app.get('/download', (req, res) => {
-  // console.log('req.requestTime = ', req.requestTime)
-  res.download(path.resolve(__dirname, 'static', 'index.html'))
-})
+// app.get('/download', (req, res) => {
+//   // console.log('req.requestTime = ', req.requestTime)
+//   res.download(path.resolve(__dirname, 'static', 'index.html'))
+// })
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`)
