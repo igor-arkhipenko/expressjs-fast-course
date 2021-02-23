038dae97a709e9987ae7f3a8be64d97564521453
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-23 15:39:49 +0300

    middlewares

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import express from 'express'
+import express, {request} from 'express'
 import path from 'path'
+import { requestTime, logger } from './middlewares.js'
 
 const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 // })
 
 app.get('/download', (req, res) => {
+  // console.log('req.requestTime = ', req.requestTime)
   res.download(path.resolve(__dirname, 'static', 'index.html'))
 })
 
